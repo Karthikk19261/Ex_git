916698b791d1fecfeb094cc3f37aad22ef10e0e0
Karthik Kishore <karthikkishore19261@gmail.com>
2024-09-03 09:47:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_git
+{
+    public class Program
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Sub(5, 3));
+            Console.ReadLine();
+        }
+    }
+}
